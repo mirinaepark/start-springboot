64095277152ca0b3c1ba9037ee2b3df890978b54
DESKTOP-4VRO4BB\qkr63 <goroweon@gmail.com>
2022-05-22 14:40:26 +0900

    spring boot projcet 생성

---
+++ src/main/java/com/mrnstudio/spring/JavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mrnstudio.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringApplication.class, args);
+	}
+
+}
